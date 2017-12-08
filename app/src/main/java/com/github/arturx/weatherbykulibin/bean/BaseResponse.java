da44e51059697004c25a6a92a501e810e3e2ec6c
arturx <abdurahmanovart1994@gmail.com>
2017-12-09 01:47:34 +0300

    added getters and overridden methods to BaseResponse class

--- app/src/main/java/com/github/arturx/weatherbykulibin/bean/BaseResponse.java
+++ app/src/main/java/com/github/arturx/weatherbykulibin/bean/BaseResponse.java
@@ -1,7 +1,10 @@
 package com.github.arturx.weatherbykulibin.bean;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
 
 import java.util.List;
 
@@ -20,5 +23,36 @@ public class BaseResponse {
     @JsonProperty("list")
     private List<WeatherData> mDataList;
 
+    public int getResultCount() {
+        return resultCount;
+    }
 
+    public List<WeatherData> getDataList() {
+        return mDataList;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseResponse that = (BaseResponse) o;
+        return resultCount == that.resultCount &&
+                Objects.equal(mDataList, that.mDataList);
+    }
+
+    @JsonIgnore
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(resultCount, mDataList);
+    }
+
+    @JsonIgnore
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("resultCount", resultCount)
+                .add("mDataList", mDataList)
+                .toString();
+    }
 }
