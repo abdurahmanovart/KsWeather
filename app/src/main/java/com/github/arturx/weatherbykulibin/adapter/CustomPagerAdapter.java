e210201fa11adde17c02c918013a8edd40f4899b
arturx <abdurahmanovart1994@gmail.com>
2017-12-13 00:22:13 +0300

    added adapter for viewpager

--- app/src/main/java/com/github/arturx/weatherbykulibin/adapter/CustomPagerAdapter.java
+++ app/src/main/java/com/github/arturx/weatherbykulibin/adapter/CustomPagerAdapter.java
@@ -4,7 +4,10 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
+import com.github.arturx.weatherbykulibin.CurrentWeatherFragment;
+import com.github.arturx.weatherbykulibin.ThreeDayWeatherFragment;
 import com.github.arturx.weatherbykulibin.TodayWeatherFragment;
+import com.github.arturx.weatherbykulibin.bean.WeatherData;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,25 +18,37 @@ import java.util.List;
 
 public class CustomPagerAdapter extends FragmentStatePagerAdapter {
 
-    private List<TodayWeatherFragment> mList;
+    private List<WeatherData> mWeatherDataList;
+
+    public void setWeatherDataList(List<WeatherData> weatherDataList) {
+        mWeatherDataList = weatherDataList;
+    }
 
     public CustomPagerAdapter(FragmentManager fm) {
         super(fm);
-        mList = new ArrayList<>();
     }
 
     @Override
     public Fragment getItem(int position) {
-        return mList.isEmpty() ? null : mList.get(position);
+            switch (position) {
+                case 0:
+                    return CurrentWeatherFragment.newInstance(mWeatherDataList.get(0));
+                case 1:
+                    return TodayWeatherFragment.newInstance(mWeatherDataList);
+                case 2:
+                    return ThreeDayWeatherFragment.newInstance(mWeatherDataList);
+                default:
+                    return null;
+        }
     }
 
     @Override
     public int getCount() {
-        return mList.size();
+        return 3;
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        return mList.get(position).getTitle();
+       return String.valueOf(mWeatherDataList.get(position).getWindData().getSpeed());
     }
 }
