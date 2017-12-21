39d61b9fb12f8bc6b84a6626aa795e4aa14917c7
arturx <abdurahmanovart1994@gmail.com>
2017-12-21 22:54:53 +0300

    created Utils class

---
+++ app/src/main/java/com/github/arturx/weatherbykulibin/utils/Utils.java
@@ -0,0 +1,28 @@
+package com.github.arturx.weatherbykulibin.utils;
+
+import com.github.arturx.weatherbykulibin.R;
+
+/**
+ * Created by arturx on 13.12.17.
+ */
+
+public class Utils {
+
+
+    public static int getWeatherImageId(String description) {
+        description = description.toLowerCase();
+        if (description.contains("rain"))
+            return R.drawable.icon_rainy_weather;
+        else if (description.contains("thunder") || description.contains("storm"))
+            return R.drawable.icon_thunder_weather;
+        else if (description.contains("clear"))
+            return R.drawable.icon_clear_day;
+        else if (description.contains("cloud"))
+            return R.drawable.icon_cloudy;
+        else if (description.contains("wind"))
+            return R.drawable.icon_windy_weather;
+        else if (description.contains("snow"))
+            return R.drawable.icon_snow_weather;
+        return R.drawable.icon_mostly_cloudy;
+    }
+}
