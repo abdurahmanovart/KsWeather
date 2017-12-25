98a000d577023cee6a1a786faa5c5ee2d009aa4d
arturx <abdurahmanovart1994@gmail.com>
2017-12-26 01:37:55 +0300

    added on click handling to google map

--- app/src/main/java/com/github/arturx/weatherbykulibin/MapFragment.java
+++ app/src/main/java/com/github/arturx/weatherbykulibin/MapFragment.java
@@ -1,5 +1,7 @@
 package com.github.arturx.weatherbykulibin;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,22 +12,27 @@ import android.view.ViewGroup;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by arturx on 22.12.17.
  */
 
-public class MapFragment extends Fragment implements GoogleMap.OnMapClickListener{
+public class MapFragment extends Fragment implements GoogleMap.OnInfoWindowClickListener,
+        GoogleMap.OnMapClickListener,
+        GoogleMap.OnMapLongClickListener {
 
-    private MapView mMapView;
     private GoogleMap mGoogleMap;
+    private MapView mMapView;
 
     public MapFragment() {
 
     }
 
-    public static MapFragment newInstance(){
+    public static MapFragment newInstance() {
         MapFragment fragment = new MapFragment();
         return fragment;
     }
@@ -38,17 +45,25 @@ public class MapFragment extends Fragment implements GoogleMap.OnMapClickListene
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_map,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
         mMapView = rootView.findViewById(R.id.map_view);
         mMapView.onCreate(savedInstanceState);
         mMapView.onResume();
-        try{
+        try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        mMapView.setCameraDistance(100.0f);
-       return rootView;
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(final GoogleMap googleMap) {
+                mGoogleMap = googleMap;
+                mGoogleMap.setOnMapClickListener(MapFragment.this);
+                mGoogleMap.setOnMapLongClickListener(MapFragment.this);
+                mGoogleMap.setOnInfoWindowClickListener(MapFragment.this);
+            }
+        });
+        return rootView;
     }
 
     @Override
@@ -76,7 +91,29 @@ public class MapFragment extends Fragment implements GoogleMap.OnMapClickListene
     }
 
     @Override
-    public void onMapClick(LatLng latLng) {
-
+    public void onInfoWindowClick(Marker marker) {
+        double mLongitude = marker.getPosition().longitude;
+        double mLatitude = marker.getPosition().latitude;
+        Activity activity = getActivity();
+        Intent intent = new Intent();
+        intent.putExtra(MainActivity.EXTRA_LONGITUDE, mLongitude);
+        intent.putExtra(MainActivity.EXTRA_LATITUDE, mLatitude);
+        activity.setResult(MainActivity.RESULT_FROM_MAP, intent);
+        activity.finish();
     }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        mGoogleMap.clear();
+        mGoogleMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .title("Узнать погоду здесь?"))
+                .showInfoWindow();
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        mGoogleMap.clear();
+    }
+
 }
