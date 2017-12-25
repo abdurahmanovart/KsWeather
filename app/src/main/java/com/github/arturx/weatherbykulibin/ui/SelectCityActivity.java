21f98fef1df4b3ec7aa8187e1b02dbf4e82e61d0
arturx <abdurahmanovart1994@gmail.com>
2017-12-26 02:26:01 +0300

    changed text input field to searchview

--- app/src/main/java/com/github/arturx/weatherbykulibin/ui/SelectCityActivity.java
+++ app/src/main/java/com/github/arturx/weatherbykulibin/ui/SelectCityActivity.java
@@ -1,6 +1,7 @@
 package com.github.arturx.weatherbykulibin.ui;
 
 import android.app.AlertDialog;
+import android.app.SearchManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -9,6 +10,8 @@ import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -16,7 +19,7 @@ import android.widget.LinearLayout;
 import com.github.arturx.weatherbykulibin.MapFragment;
 import com.github.arturx.weatherbykulibin.R;
 
-public class SelectCityActivity extends AppCompatActivity {
+public class SelectCityActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     private static final String EXTRA_CITY_NAME = "city_name";
     private TextInputEditText mEditText;
@@ -24,6 +27,8 @@ public class SelectCityActivity extends AppCompatActivity {
     private Button mShowMapButton;
     private TextInputLayout mTextInputLayout;
     private LinearLayout mPlaceForMapLayout;
+    private SearchView mSearchView;
+    private Toolbar mToolbar;
 
     public static Intent createExplicitIntent(Context context) {
         return new Intent(context, SelectCityActivity.class);
@@ -56,39 +61,46 @@ public class SelectCityActivity extends AppCompatActivity {
                 .create()
                 .show();
     }
+    
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_CITY_NAME, query);
+        setResult(RESULT_OK, intent);
+        finish();
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
 
     private void initUI() {
-        initTextFields();
+        initToolBar();
         initButtons();
         initPlaceForMapLayout();
+    }
+
+    private void initToolBar() {
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        initSearchView();
+    }
+
+    private void initSearchView() {
+        mSearchView = findViewById(R.id.search);
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        mSearchView.setOnQueryTextListener(this);
     }
 
     private void initPlaceForMapLayout() {
         mPlaceForMapLayout = findViewById(R.id.place_for_map_layout);
     }
 
-    private void initTextFields() {
-        mEditText = findViewById(R.id.input_city_edit_text);
-        mTextInputLayout = findViewById(R.id.text_input_layout);
-    }
-
     private void initButtons() {
-        mOkButton = findViewById(R.id.ok_button);
         mShowMapButton = findViewById(R.id.show_map_button);
-        mOkButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String cityName = mEditText.getText().toString();
-                if (cityName.length() < 3) {
-                    showAlertDialog();
-                } else {
-                    Intent intent = new Intent();
-                    intent.putExtra(EXTRA_CITY_NAME, cityName);
-                    setResult(RESULT_OK, intent);
-                    finish();
-                }
-            }
-        });
         mShowMapButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -101,17 +113,20 @@ public class SelectCityActivity extends AppCompatActivity {
         });
     }
 
-    private void showAlertDialog() {
-        new AlertDialog.Builder(this)
-                .setTitle(R.string.enter_city)
-                .setMessage(R.string.enter_correct_city_name)
-                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                })
-                .create()
-                .show();
-    }
+//    }
+
+    //    private void showAlertDialog() {
+//        new AlertDialog.Builder(this)
+//                .setTitle(R.string.enter_city)
+//                .setMessage(R.string.enter_correct_city_name)
+//                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//                    }
+//                })
+//                .create()
+//                .show();
+
+
 }
